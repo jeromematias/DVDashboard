bcfc98a9049d085c28b6a7c0d48fc8e97ac8127d
jeromematias <jbm@datafactorycorp.com>
2018-04-06 11:52:16 +0800

    update

--- app/Http/Controllers/dbcontroller.php
+++ app/Http/Controllers/dbcontroller.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class dbcontroller extends Controller
 {
-    //
+	protected $surveylist = array();
+  public function __construct(){
+  	$this->surveylist = Surveys::all();
+  }
+  public function getSurveys(){  	
+  	return response($this->surveylist);
+  }
 }
