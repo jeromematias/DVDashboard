f6b5686680cb05d013d43a088fff4857483d227e
jeromematias <jbm@datafactorycorp.com>
2018-04-06 15:30:02 +0800

    update

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -40,8 +40,8 @@ $(function(){
 				if($('#surveynames').val() == data[i].SurveyName){
 					output += '<tr>'+
 											'<td>'+ showeekday(data[i].WeekDay) +'</td>'+
-											'<td class="text-center">'+ data[i].MinsBefore +'<i class="icon-note"></i></td>'+
-											'<td class="text-center">'+ data[i].MinsAfter +'<i class="icon-note"></td>';
+											'<td class="text-center">'+ data[i].MinsBefore +' <i class="icon-note float-right"></i></td>'+
+											'<td class="text-center">'+ data[i].MinsAfter +' <i class="icon-note float-right"></td>';
 					output += '</tr>'
 				}
 			}
