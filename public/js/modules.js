3b7e934e00e97097332767eb05f1be1487527d5b
jeromematias <jbm@datafactorycorp.com>
2018-04-06 12:18:16 +0800

    update

--- public/js/modules.js
+++ public/js/modules.js
@@ -2,6 +2,6 @@ $.fn.appendSurvey = function(Survey){
 	var self = this;	
 	for(var i in Survey.Surveylist){
 		var surveyname = Survey.Surveylist[i]
-		$(self).append(surveyname)
+		$(self).append('<li class="nav-item"><a class="nav-link" href="#"><i class="icon-plus text-success font-weight-bold"></i> '+ surveyname +'</a></li>')
 	}
 }
