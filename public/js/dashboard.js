3c2054d3a5d6ef60abc3c50707f9c0303624035d
jeromematias <jbm@datafactorycorp.com>
2018-04-06 16:21:48 +0800

    update

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -73,7 +73,7 @@ $(function(){
 			$('#tb-conversion tbody tr td #up-minb').each(function(){
 				$(this).click(function(){
 					//alert($(this).data('value'))
-					$('#modal-timeframe').modal('show')
+					$('#modal-timeframe').modal()
 				})
 				$(this).css({
 					cursor : 'pointer'
