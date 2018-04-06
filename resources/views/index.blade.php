78c2f36a0846a0d7367fe9de5f8545142e139a5c
jeromematias <jbm@datafactorycorp.com>
2018-04-06 16:05:22 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,7 +47,7 @@
     <div class="modal-content" style="height : 100%;">
       <!-- Modal Header -->
       <div class="modal-header bg-primary">
-        <strong class="modal-title">Update</strong>
+        <h5 class="modal-title">Update</h5>
         <button type="button" class="close" data-dismiss="modal">&times;</button>
       </div>
       <!-- Modal body -->
