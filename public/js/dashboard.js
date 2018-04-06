df75a7edda480e53e142bed91c9d017b427f2104
jeromematias <jbm@datafactorycorp.com>
2018-04-06 14:19:28 +0800

    update

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,18 +1,18 @@
 $(function(){
 	var Surveylist = [];
-	init_surveynames();
+	$.get(window.location.href + "Surveys",function(data){		
+		for(var i in data){
+			Surveylist.push(data[i].SurveyName)
+		}
+	}).done(function(){
+		init_surveynames();
+	})
 	function init_surveynames(){
-		$.get(window.location.href + "Surveys",function(data){		
-			for(var i in data){
-				Surveylist.push(data[i].SurveyName)
-			}
-		}).done(function(){
-			$('#surveylist').appendSurvey({
-				Surveylist : Surveylist
-			})
-			$('#surveynames').appendSurveyOption({
-				Surveylist : Surveylist
-			})
+		$('#surveylist').appendSurvey({
+			Surveylist : Surveylist
+		})
+		$('#surveynames').appendSurveyOption({
+			Surveylist : Surveylist
 		})
 	}		
 });
