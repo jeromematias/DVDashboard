d4bc59f203780879d5626c96991ac1d7b00c1be0
jeromematias <jbm@datafactorycorp.com>
2018-04-06 12:06:38 +0800

    update

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,16 +2,4 @@ $(function(){
 	$('#surveylist').appendSurvey({
 		Surveylist : ['Singtel Cencus','Singtel NewsRoom','Digiturk','SKY UK Cencus']
 	})
-	$.get(window.location.href + "Surveys",{},function(data){
-		console.log(data);
-	})
-	$.ajax({
-		type : 'GET',
-		url : window.location.href + "Surveys",
-		success : function(data){
-			console.log(data);
-		}
-	})
-	
-
 });
