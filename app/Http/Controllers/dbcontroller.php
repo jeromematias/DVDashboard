647c59dfb16979a5a8aa2df7c2c93ec49f15f83d
jeromematias <jbm@datafactorycorp.com>
2018-04-06 14:27:35 +0800

    update

--- app/Http/Controllers/dbcontroller.php
+++ app/Http/Controllers/dbcontroller.php
@@ -9,15 +9,22 @@ class dbcontroller extends Controller
 	
 	protected $surveylist = array();
 	protected $timeframes = array();
-
+	/**
+	 * [__construct description]
+	 */
   public function __construct(){
   	$this->surveylist = Surveys::all();
   }
-  
+  /**
+   * [getSurveys description]
+   * @return [type] [description]
+   */
   public function getSurveys(){  	
   	return response($this->surveylist);
   }
-
+  /**
+   * [GetSurveyTimeFrames description]
+   */
   public function GetSurveyTimeFrames(){
   	$this->timeframes = DB::select('EXEC GetSurveyTimeFrames');
     return response($this->timeframes);
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,7 +6,26 @@ $(function(){
 		}
 	}).done(function(){
 		init_surveynames();
+		showTimeFrames();
 	})
+	
+
+	/**
+	 * [showTimeFrames description]
+	 * @return {[type]} [description]
+	 */	
+	function showTimeFrames(){
+		$.get(window.location.href + "timeframes",function(data){		
+			console.log(data)			
+		}).done(function(){
+			
+		})	
+	}
+
+	/**
+	 * [init_surveynames description]
+	 * @return {[type]} [description]
+	 */	
 	function init_surveynames(){
 		$('#surveylist').appendSurvey({
 			Surveylist : Surveylist
