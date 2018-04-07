3b1eb2f4471970c0256f91c5ccf675c4cfcbea42
jeromematias <jbm@datafactorycorp.com>
2018-04-07 12:05:17 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,8 +34,8 @@
     </ul>
     <div class="tab-content">
       <div class="tab-pane active" id="home" role="tabpanel">
-        <div class="row col-sm-5 ml-auto border" style="height: 70px;margin-top: 0px;">
-          <div class="form-group col-sm-6">
+        <div class="row col-sm-3 ml-auto border" style="height: 70px;margin-top: 0px;">
+          <div class="form-group col-sm-9">
             <label class="col-form-label col-form-label-sm col-sm-12" for="unit">Unit</label>
             <div class="col-sm-12">
               <select id="surveynames" name="surveynames" class="form-control form-control-sm">
@@ -43,7 +43,7 @@
               </select>
             </div>
           </div>
-          <div class="form-group  col-sm-6">
+          <div class="form-group  col-sm-3">
             <label class="col-form-label  col-form-label-sm col-sm-12" for="unit">Unit</label>
             <div class="col-sm-12">
               <select id="surveynames" name="surveynames" class="form-control form-control-sm">
