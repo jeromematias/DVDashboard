ab5ea1fc0b1fffc03397719cd73f45f2988b491e
jeromematias <jbm@datafactorycorp.com>
2018-04-07 15:13:21 +0800

    update

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -144,10 +144,10 @@ $(function(){
 	}
 
 	function arraysEqual(arr1, arr2) {
-    if(arr1.length != arr2.length)
+    if(arr1.length !== arr2.length)
       return false;
     for(var i = arr1.length; i--;) {
-      if(arr1[i] != arr2[i])
+      if(arr1[i] !== arr2[i])
         return false;
     }
     console.log("graph",{status : 'updaterequired'})
