ddee5034a9adac775c478accafcb8571fe4c2733
jeromematias <jbm@datafactorycorp.com>
2018-04-07 11:58:49 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,7 +34,7 @@
     </ul>
     <div class="tab-content">
       <div class="tab-pane active" id="home" role="tabpanel">
-        <div class="row col-sm-5 ml-auto border" style="height: 50px;">
+        <div class="row col-sm-5 ml-auto border" style="height: 50px;margin-top: 0px;">
           <div class="form-group">
             <label class="col-form-label col-sm-12" for="unit">Unit</label>
             <div class="col-sm-12">
