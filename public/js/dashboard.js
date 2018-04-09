572caf333fb1200570f548f31b61932a42d09201
jeromematias <jbm@datafactorycorp.com>
2018-04-09 10:25:52 +0800

    update

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -334,6 +334,7 @@ $(function(){
 			        data: seriesData,
 			        type: 'bar',
 			        label: {
+			        		color : '#000',
 	                normal: {
                     show: true,
                     color : '#000',
